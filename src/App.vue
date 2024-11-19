b2ecb70d3f538f3f9eafa27c1a1a8c5b01286c99
ZalmaCastroRosas <janetzincastro@outlook.com>
2024-11-18 18:51:36 -0600

    Uso de expresiones JS

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
 const name = ', mi primer proyecto';
 const styleColor = 'color: green';
+const arrayColores = [ "blue", "red","pink", "purple" ]
+const activo = true;
 </script>
 
 <template>
   <!--<h1 v-bind:style="styleColor">Hola Zalma{{ name }}</h1>-->
   <h1 :style="styleColor">Hola Zalma{{ name }}</h1>
+  <h1 :style="styleColor">Hola Zalma{{ name.toUpperCase ()}}</h1>
+  <h2 :style="`color: ${arrayColores[3]}`">Prueba de color</h2> <!--Interpolacion-->
+  <h2>{{ activo ? "Estoy activo" : "Estoy inactivo" }}</h2>
 </template>
 
 <style>
